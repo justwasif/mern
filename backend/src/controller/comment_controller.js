7d96bf72b6992703c0d27a71774065faf3c252ab
wasif <wasif.iit.ansari@gmail.com>
2026-04-23 12:40:44 +0530

    uo]pdated :comment whit correct api

--- backend/src/controller/comment_controller.js
+++ backend/src/controller/comment_controller.js
@@ -1,61 +1,75 @@
-import { Comment, Comment } from "../models/Comment.js";
+import { Comment } from "../models/Comment.js";
 import { ApiError } from "../utils/APiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const createComment=asyncHandler(async(req,res)=>{
-    try{
-        const {post_id,content}=req.body;
-        if(!post_id||!content){
-            return res.status(400).ApiResponse(400,"entre req feils");
-        }
 
-        const comment=await Comment.create({
-            user_id:req.user_id,
-            post_id,
-            content
-        });
-        res.status(201).ApiResponse(201,"comment created");
+const createComment = asyncHandler(async (req, res) => {
+  try {
+    const { post_id, content } = req.body;
 
-
-    } catch(err){
-        res.status(400).ApiError(400,err.message);
-
+    if (!post_id || !content) {
+      return res.status(400).json(new ApiError(400, "enter required fields"));
     }
+
+    const comment = await Comment.create({
+      user_id: req.user_id,
+      post_id,
+      content
+    });
+
+    return res
+      .status(201)
+      .json(new ApiResponse(201, comment, "comment created"));
+  } catch (err) {
+    return res.status(500).json(new ApiError(500, err.message));
+  }
 });
 
-const getCommentByPost=asyncHandler(async(req,res)=>{
-    try{
-        const {posId}=req.prams;
-        const comment =await comment.find({
-            pos_id:posId,
-            is_deleted:false
-        }).populate("user_id","name").sort({createdAt:-1});
-        res.status(200).ApiResponse(200,comment);
-    }catch (err){
-        res.status(500).ApiError(500,err.message);
-    }
-})
 
-const deleteComment=asyncHandler(async(req,res)=>{
-    try{
-        const {id}=req.prams;
-        const comment=await Comment.findBy(id);
-        if(!comment){
-            return res.status(404).ApiResponse(404,"no comment");
-        }
-        if(comment.user_id.toString() !==req.user_id){
-            return res.status(403).ApiResponse(403,"un");
-        }
-        comment.is_deleted=true;
-        await comment.save();
-        res.status(200).ApiResponse(200,"comment successfully removed");
+const getCommentByPost = asyncHandler(async (req, res) => {
+  try {
+    const { postId } = req.params;
 
-    }catch(err){
-        res.status(500).ApiError(500,err.message);
-    }
+    const comments = await Comment.find({
+      post_id: postId,
+      is_deleted: false
+    })
+      .populate("user_id", "name")
+      .sort({ createdAt: -1 });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, comments));
+  } catch (err) {
+    return res.status(500).json(new ApiError(500, err.message));
+  }
 });
 
-export {createComment,deleteComment,getCommentByPost};
 
+const deleteComment = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
 
+    const comment = await Comment.findById(id);
+
+    if (!comment) {
+      return res.status(404).json(new ApiError(404, "no comment"));
+    }
+
+    if (comment.user_id.toString() !== req.user_id) {
+      return res.status(403).json(new ApiError(403, "unauthorized"));
+    }
+
+    comment.is_deleted = true;
+    await comment.save();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, null, "comment successfully removed"));
+  } catch (err) {
+    return res.status(500).json(new ApiError(500, err.message));
+  }
+});
+
+export { createComment, deleteComment, getCommentByPost };
